19ef3bfa68d48e91e173a49710805b927fb6894f
Djaypm89 <djaypm89@gmail.com>
2021-12-01 12:58:53 -0500

    create song (post method) complete

--- index.js
+++ index.js
@@ -23,9 +23,15 @@ app.get('/api/songs', (req, res) => {
     return res.send(songs)
 });
 
-app.get('/api/songs/:id', (req,res) => {
+app.get('/api/songs/:id', (req, res) => {
     console.log("GET song by ID triggered!")
     const id = req.params.id;
     const song = repoContext.songs.findSongById(id);
     return res.send(song);
+});
+
+app.post('/api/songs', (req, res) => {
+    const newSong = req.body;
+    const addedSong = repoContext.songs.createSong(newSong);
+    return res.send(addedSong);
 });
