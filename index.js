f8559de0b70c4346d27d4b2425d5e88b99cda0e1
Djaypm89 <djaypm89@gmail.com>
2021-12-01 12:17:29 -0500

    get all songs functions

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const express = require("express");
 
+const repoContext = require('./repository/repository-wrapper');
+const cors = require('cors');
+
+
+
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.listen(3001, function () {
-    console.log("Server started. Listening on port 3000");
+    console.log("Server started. Listening on port 3001");
 });
+
+
+
+
+app.get('/api/songs', (req, res) => {
+    console.log("GET all songs triggered!")
+    const songs = repoContext.songs.findAllSongs()
+    return res.send(songs)
+})
